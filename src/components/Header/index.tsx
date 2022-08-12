9c3fe7cb9d630dee5b70f34c3b46ed71b8633e14
Samuel Jansem <samuel.jansem@gmail.com>
2022-08-12 11:31:53 -0300

    header component auth functionalities

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,44 @@
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
+import { useAuth } from '../../hooks/useAuth';
 
 export function Header() {
+    const { authenticated, user, logout } = useAuth();
+
+    const handleLogout = () => {
+        logout();
+    };
+
     return (
         <header className={styles.header}>
-            <span>
-                <Link to="/">Wall</Link>
+            <span className={styles.lg}>
+                <Link to="/">Wallapp</Link>
             </span>
-            <span>
-                <Link to="/login">Login</Link>
-            </span>
-            <span>
-                <Link to="/signup">Signup</Link>
-            </span>
+            <div className={styles.links}>
+                {authenticated ? (
+                    <>
+                        <span className={styles.sm}>
+                            <Link to={`/users/${user?.username}`}>
+                                Hi, {user?.username}
+                            </Link>
+                        </span>
+                        <span className={styles.sm}>
+                            <a href="#" onClick={handleLogout}>
+                                Logout
+                            </a>
+                        </span>
+                    </>
+                ) : (
+                    <>
+                        <span className={styles.sm}>
+                            <Link to="/login">Login</Link>
+                        </span>
+                        <span className={styles.sm}>
+                            <Link to="/signup">Signup</Link>
+                        </span>
+                    </>
+                )}
+            </div>
         </header>
     );
 }
