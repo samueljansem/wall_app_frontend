2a98679e990515dfdf2eda33da21b2ed1d05b577
Samuel Jansem <samuel.jansem@gmail.com>
2022-08-12 11:32:05 -0300

    add login form

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,20 +1,50 @@
+import { FormEvent, useRef, useState } from 'react';
+import { useAuth } from '../../hooks/useAuth';
+import { UserLoginInput } from '../../@types/auth';
+import styles from './styles.module.css';
+
 export function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const formRef = useRef<HTMLFormElement>(null);
+
+    const { login } = useAuth();
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        await login({ username, password } as UserLoginInput);
+        formRef.current?.reset();
+    };
+
     return (
-        <section>
-            <h3>Login Form</h3>
-            <form>
-                <div>
-                    <input type="text" name="username" placeholder="Username" />
-                </div>
-                <div>
+        <section className={styles.wrapper}>
+            <h3>Login</h3>
+            <hr />
+            <form className={styles.form} onSubmit={handleSubmit} ref={formRef}>
+                <div className={styles.row}>
                     <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
+                        type="text"
+                        placeholder="Username"
+                        name="username"
+                        onChange={(e) => {
+                            setUsername(e.target.value);
+                        }}
                     />
                 </div>
-                <div>
-                    <button type="submit">Login</button>
+                <div className={styles.row}>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                    />
+                </div>
+                <div className={styles.row}>
+                    <button type="submit" className={styles.submit}>
+                        Login
+                    </button>
                 </div>
             </form>
         </section>
