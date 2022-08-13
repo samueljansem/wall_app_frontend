a803fdfe155e0bd3dbdd4b9b954f710e1d1498b6
Samuel Jansem <samuel.jansem@gmail.com>
2022-08-13 18:06:39 -0300

    route redirection is user has signedin

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from 'react-router-dom';
 import App from '../App';
-import { Wall, Login, SignUp, NotFound } from '../pages';
+import { Wall, Login, SignUp, NotFound, Profile } from '../pages';
+import { useAuth } from '../hooks/useAuth';
 
 export function AppRouter() {
+    const { authenticated } = useAuth();
     return (
         <Router>
             <Routes>
                 <Route element={<App />} path="/">
                     <Route element={<Wall />} index />
-                    <Route element={<Login />} path="/login" />
-                    <Route element={<SignUp />} path="/signup" />
+                    <Route
+                        element={
+                            authenticated ? <Navigate to="/" /> : <Login />
+                        }
+                        path="/login"
+                    />
+                    <Route
+                        element={
+                            authenticated ? <Navigate to="/" /> : <SignUp />
+                        }
+                        path="/signup"
+                    />
+                    <Route path="/users">
+                        <Route element={<Profile />} path=":username" />
+                    </Route>
                     <Route element={<NotFound />} path="*" />
                 </Route>
             </Routes>
